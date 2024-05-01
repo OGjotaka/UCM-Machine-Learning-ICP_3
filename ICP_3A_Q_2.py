f3b9cac591bb1d4ec430c3f480b3c262408a483d
OGjotaka <68663897+OGjotaka@users.noreply.github.com>
2024-04-30 19:09:41 -0500

    Add files via upload

---
+++ ICP_3A_Q_2.py
@@ -0,0 +1,26 @@
+#CS 4710
+#JJ Holbrook
+#700514202
+#ICP 3 Part A Question 2
+
+'''
+Write a program to compute the eigenvalues and right eigenvectors of a given square array given below:
+[[ 3 -2]
+[ 1 O]]
+'''
+import numpy as np
+from scipy import linalg
+
+a = np.array([[3, -2],[1, 0]])
+print(a)
+'''
+print(linalg.eigvals(a))
+print("Eigenvalues = ", real.linalg.eigvals(a))]
+'''
+# Compute and print the eigenvalues
+eigenvalues = linalg.eigvals(a)
+print("Eigenvalues without isolating real parts:", eigenvalues)
+
+# Isolate the real parts of the eigenvalues
+real_parts = np.real(eigenvalues)
+print("Eigenvalues with real parts isolated:", real_parts)
