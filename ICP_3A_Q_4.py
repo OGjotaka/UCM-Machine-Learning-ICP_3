496df6a2f5a27fb81b371bb3b594aab1a62945fa
OGjotaka <68663897+OGjotaka@users.noreply.github.com>
2024-05-02 17:19:05 -0500

    Add files via upload

---
+++ ICP_3A_Q_4.py
@@ -0,0 +1,31 @@
+#CS 4710
+#JJ Holbrook
+#700514202
+#ICP 3 Part A Question 4
+
+'''
+Write a NumPy program to create a new shape to an array without changing its data. Reshape 3x2:
+[[l 2]
+[3 4]
+[5 6]]
+Reshape 2x3:
+[[l 2 3]
+[4 5 6]]
+'''
+import numpy as np
+
+# Original array
+original_array = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+
+# Reshape to 3x2
+reshaped_3x2 = original_array.reshape(3, 2)
+print("Reshaped to 3x2:")
+print(reshaped_3x2)
+
+# Reshape to 2x3
+reshaped_2x3 = original_array.reshape(2, 3)
+print("\nReshaped to 2x3:")
+print(reshaped_2x3)
+
